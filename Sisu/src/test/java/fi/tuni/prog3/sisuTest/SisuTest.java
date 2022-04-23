1eaa7331889f7d406650b68cf1d63e344378a27f
Onni Merilä <onni.merila@gmail.com>
2022-04-23 04:54:31 +0300

    Edited test class structure -Onni

---
+++ Sisu/src/test/java/fi/tuni/prog3/sisuTest/SisuTest.java
@@ -0,0 +1,38 @@
+package fi.tuni.prog3.sisuTest;
+/*
+    @author - Onni Merila , onni.merila@tuni.fi , H299725
+ */
+
+import fi.tuni.prog3.sisu.Main;
+import javafx.stage.Stage;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class SisuTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        new Main().start(stage);
+
+    }
+
+    @Override public void stop() {
+
+    }
+
+    //TODO: Klikkeritestit
+
+    //TODO: Tiedustelutestit
+
+    //TODO: Robottitestit
+
+
+    public static void main(String[] args) throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(Main.class);
+    }
+
+
+
+
+}
