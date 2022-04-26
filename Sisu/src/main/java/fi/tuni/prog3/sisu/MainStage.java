742af8d2151a2cfe7723d4039532f0c2dd44ae2a
Onni Merilä <onni.merila@gmail.com>
2022-04-26 22:01:00 +0300

    Organized MainStage to have every tab separately as a nested class to make everything more organized and readable for later use. -Onni

--- Sisu/src/main/java/fi/tuni/prog3/sisu/MainStage.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/MainStage.java
@@ -18,151 +18,207 @@ import java.util.stream.Collectors;
 
 public class MainStage {
 
+    private Student student;
+    private List<Degree> degrees;
+    private TabPane tabPane;
 
-    TreeView<String> treeView;
+
     HashMap<String, List<String>> testTreeItems = new HashMap<>();
     List<String> testList = Arrays.asList("Kurssi1", "Kurssi2", "Kurssi3", "Kurssi4");
-    private TreeItem<String> rootNode;
 
-    private void makeTreeView(Degree degree){
-        rootNode = new TreeItem<>(degree.getName());
-        for(var treeItem : testTreeItems.entrySet()) {
-            TreeItem<String> webItem = new TreeItem<>(treeItem.getKey());
-            for(var item : treeItem.getValue()) {
-                webItem.getChildren().add(new TreeItem<>(item));
-            }
-            rootNode.getChildren().add(webItem);
-        }
-        treeView.setRoot(rootNode);
-    }
-
-    //Home page.
-    Label greetingLabel = new Label();
-
-    // Course page.
-    Button changeDegreeButton = new Button("Vaihda");
-
-    // Personal page.
-    Label nameInfoLabel = new Label("Koko nimi");
-    Label nameLabel = new Label();
-    Label studentNumberInfoLabel = new Label("Opiskelijanumero");
-    Label studentNumberLabel = new Label();
-    Label emailInfoLabel = new Label("Sähköpostiosoite");
-    Label emailLabel = new Label();
 
     MainStage(Stage stage, Student student, List<Degree> degrees) {
-        // Making the degree drop box.
-        List<String> degreeNames = degrees.stream().map(Degree::getName).collect(Collectors.toList());
-        ObservableList<String> degreeObsList = FXCollections.observableArrayList(degreeNames);
-        final SearchableComboBox<String> degreeComboBox = new SearchableComboBox<>(degreeObsList);
+        // Initializing stuff
+        this.student = student;
+        this.degrees = degrees;
 
-        // Setting personal texts.
-        greetingLabel.setText(String.format("Tervetuloa Sisuun %s!", student.getFirstName()));
-        nameLabel.setText(student.getName());
-        studentNumberLabel.setText(student.getStudentNumber());
-        emailLabel.setText(student.getEmailAddress());
-        degreeComboBox.setPromptText("Voit vaihtaa tästä tutkinnon");
+        // Creating container.
+        VBox layout = new VBox(15);
 
         // Preparing tabs.
-        TabPane tabPane = new TabPane();
+        tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        Tab homeTab = new Tab("Etusivu");
-        Tab designTab = new Tab("Omat suoritukset");
-        Tab courseTab = new Tab("Kurssinäkymä");
-        Tab personalTab = new Tab("Omat tiedot");
-        tabPane.getTabs().add(homeTab);
-        tabPane.getTabs().add(designTab);
-        tabPane.getTabs().add(courseTab);
-        tabPane.getTabs().add(personalTab);
+
+        // Creating tabs and adding them to tabPane
+        tabPane.getTabs().add(new HomeTab("Etusivu"));
+        tabPane.getTabs().add(new DesignTab("Omat suoritukset"));
+        tabPane.getTabs().add(new CourseTab("Kurssinäkymä"));
+        tabPane.getTabs().add(new PersonalTab("Omat tiedot"));
 
         // TODO: Make treeView work.
         testTreeItems.put("Joku StudyModule1", testList);
         testTreeItems.put("Joku StudyModule2", testList);
-        treeView = new TreeView<>();
-        makeTreeView(student.getDegree());
-        treeView.setShowRoot(true);
 
-        // Creating all containers.
-        VBox homeBox = new VBox(15);
+        // Adding the tabpane to the
+        layout.getChildren().add(tabPane);
 
-        GridPane homeGrid = new GridPane();
-        homeGrid.setHgap(15);
-        homeGrid.setVgap(15);
-        homeGrid.setPadding(new Insets(15,15,15,15));
-
-        GridPane courseGrid = new GridPane();
-        courseGrid.setHgap(15);
-        courseGrid.setVgap(15);
-        courseGrid.setPadding(new Insets(15,15,15,15));
-
-        GridPane designGrid = new GridPane();
-        designGrid.setHgap(15);
-        designGrid.setVgap(15);
-        designGrid.setPadding(new Insets(15,15,15,15));
-
-        GridPane personalGrid = new GridPane();
-        personalGrid.setHgap(15);
-        personalGrid.setVgap(15);
-        personalGrid.setPadding(new Insets(15,15,15,15));
-
-        // Adding nodes to grids.
-        // node, columnIndex, rowIndex, columnSpan, rowSpan:
-        homeBox.getChildren().add(tabPane);
-        homeTab.setContent(homeGrid);
-        courseTab.setContent(courseGrid);
-        designTab.setContent(designGrid);
-        personalTab.setContent(personalGrid);
-
-        homeGrid.add(greetingLabel, 0, 0);
-
-        courseGrid.add(degreeComboBox, 0, 0, 3, 1);
-        courseGrid.add(changeDegreeButton, 4, 0);
-        courseGrid.add(treeView, 0, 2, 3, 3);
-
-        personalGrid.add(nameInfoLabel, 0, 0);
-        personalGrid.add(nameLabel, 0, 1);
-        personalGrid.add(studentNumberInfoLabel, 1, 0);
-        personalGrid.add(studentNumberLabel, 1, 1);
-        personalGrid.add(emailInfoLabel, 0, 2);
-        personalGrid.add(emailLabel, 0, 3);
-
-        // Setting css id:s.
-        homeGrid.getStyleClass().add("secBackground");
-        greetingLabel.getStyleClass().add("bigHeading");
-        homeTab.getStyleClass().add("homeIcon");
+        // Setting css id for tabPane.
         tabPane.getStyleClass().add("tabPane");
-        changeDegreeButton.getStyleClass().add("basicButton");
-        nameInfoLabel.getStyleClass().add("smallHeading");
-        studentNumberInfoLabel.getStyleClass().add("smallHeading");
-        emailInfoLabel.getStyleClass().add("smallHeading");
 
         // Setting scene and stage.
-        var scene = new Scene(homeBox, 900, 650);
+        var scene = new Scene(layout, 900, 650);
         stage.setScene(scene);
         final String style = getClass().getResource("stylesheet.css").toExternalForm();
         scene.getStylesheets().add(style);
         stage.setTitle("SISU");
         stage.show();
 
-        // Actions.
-        changeDegreeButton.setOnAction(e -> {
-            if(degreeComboBox.getValue() != null) {
-                var degreeString = degreeComboBox.getValue();
-                var degree = degrees.stream()
-                        .filter(d -> degreeString.equals(d.getName()))
-                        .collect(Collectors.toList()).get(0);
-                student.changeDegree(degree);
-
-                makeTreeView(degree);
-            }
-        });
     }
-    private class HomeTab {
-        HomeTab() {
-            GridPane homeGrid = new GridPane();
-            homeGrid.setHgap(15);
-            homeGrid.setVgap(15);
-            homeGrid.setPadding(new Insets(15,15,15,15));
+    private class HomeTab extends Tab{
+        //Buttons and other elements
+        private final Label greetingLabel;
+
+        // Constructor
+        HomeTab(String label) {
+            super(label);
+            GridPane grid = new GridPane();
+            grid.setHgap(15);
+            grid.setVgap(15);
+            grid.setPadding(new Insets(15,15,15,15));
+
+            this.setContent(grid);
+
+            greetingLabel = new Label();
+            grid.add(greetingLabel,0,0);
+
+            grid.getStyleClass().add("secBackground");
+            greetingLabel.getStyleClass().add("bigHeading");
+            greetingLabel.setText(String.format("Tervetuloa Sisuun %s!", student.getFirstName()));
+
+            grid.getStyleClass().add("secBackground");
+            greetingLabel.getStyleClass().add("bigHeading");
+            this.getStyleClass().add("homeIcon");
+
+
         }
+    }
+    private class DesignTab extends Tab{
+        //Buttons and other elements
+
+        // Constructor
+        DesignTab(String label){
+            super(label);
+            GridPane grid = new GridPane();
+            grid.setHgap(15);
+            grid.setVgap(15);
+            grid.setPadding(new Insets(15,15,15,15));
+
+            this.setContent(grid);
+
+        }
+
+    }
+
+    private class CourseTab extends Tab{
+        //Buttons and other elements
+        private final Button changeDegreeButton;
+        private TreeView<String> treeView;
+        private TreeItem<String> rootNode;
+        private final SearchableComboBox<String> degreeComboBox;
+
+        // Constructor
+        CourseTab(String label){
+            super(label);
+            List<String> degreeNames = degrees.stream().map(Degree::getName).collect(Collectors.toList());
+            ObservableList<String> degreeObsList = FXCollections.observableArrayList(degreeNames);
+            degreeComboBox = new SearchableComboBox<>(degreeObsList);
+            degreeComboBox.setPromptText("Voit vaihtaa tästä tutkinnon");
+
+            GridPane grid = new GridPane();
+            grid.setHgap(15);
+            grid.setVgap(15);
+            grid.setPadding(new Insets(15,15,15,15));
+
+            this.setContent(grid);
+
+            treeView = new TreeView<>();
+            makeTreeView(student.getDegree());
+            treeView.setShowRoot(true);
+
+            changeDegreeButton = new Button("Vaihda");
+            grid.add(degreeComboBox,0,0,3,1);
+            grid.add(changeDegreeButton,4,0);
+            grid.add(treeView,0,2,3,3);
+
+            changeDegreeButton.getStyleClass().add("basicButton");
+            changeDegreeButton.setOnAction(e -> {
+                if(degreeComboBox.getValue() != null) {
+                    var degreeString = degreeComboBox.getValue();
+                    var degree = degrees.stream()
+                            .filter(d -> degreeString.equals(d.getName()))
+                            .collect(Collectors.toList()).get(0);
+                    student.changeDegree(degree);
+
+                    makeTreeView(degree);
+                }
+            });
+
+
+
+
+        }
+
+        private void makeTreeView(Degree degree){
+            rootNode = new TreeItem<>(degree.getName());
+            for(var treeItem : testTreeItems.entrySet()) {
+                TreeItem<String> webItem = new TreeItem<>(treeItem.getKey());
+                for(var item : treeItem.getValue()) {
+                    webItem.getChildren().add(new TreeItem<>(item));
+                }
+                rootNode.getChildren().add(webItem);
+            }
+            treeView.setRoot(rootNode);
+        }
+
+
+
+    }
+
+    private class PersonalTab extends Tab{
+        //Buttons and other elements
+        private Label nameInfoLabel;
+        private Label nameLabel;
+        private Label studentNumberInfoLabel;
+        private Label studentNumberLabel;
+        private Label emailInfoLabel;
+        private Label emailLabel;
+
+
+        // Constructor
+        PersonalTab(String label){
+            super(label);
+            GridPane grid = new GridPane();
+            grid.setHgap(15);
+            grid.setVgap(15);
+            grid.setPadding(new Insets(15,15,15,15));
+
+            nameInfoLabel = new Label("Koko nimi");
+            nameLabel = new Label();
+            studentNumberInfoLabel = new Label("Opiskelijanumero");
+            studentNumberLabel = new Label();
+            emailInfoLabel = new Label("Sähköpostiosoite");
+            emailLabel = new Label();
+
+
+            grid.add(nameInfoLabel, 0, 0);
+            grid.add(nameLabel, 0, 1);
+            grid.add(studentNumberInfoLabel, 1, 0);
+            grid.add(studentNumberLabel, 1, 1);
+            grid.add(emailInfoLabel, 0, 2);
+            grid.add(emailLabel, 0, 3);
+
+            nameLabel.setText(student.getName());
+            studentNumberLabel.setText(student.getStudentNumber());
+            emailLabel.setText(student.getEmailAddress());
+
+            this.setContent(grid);
+
+            nameInfoLabel.getStyleClass().add("smallHeading");
+            studentNumberInfoLabel.getStyleClass().add("smallHeading");
+            emailInfoLabel.getStyleClass().add("smallHeading");
+
+
+        }
+
     }
 }
