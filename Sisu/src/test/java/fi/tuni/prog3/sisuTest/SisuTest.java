cd2682e764f5715b533f759a951123e9a73cd6a9
Onni Merilä <onni.merila@gmail.com>
2022-04-28 05:17:18 +0300

    Added a bunch of new tests. Yay -Onni

--- Sisu/src/test/java/fi/tuni/prog3/sisuTest/SisuTest.java
+++ Sisu/src/test/java/fi/tuni/prog3/sisuTest/SisuTest.java
@@ -6,10 +6,10 @@ package fi.tuni.prog3.sisuTest;
 import fi.tuni.prog3.sisu.Main;
 import fi.tuni.prog3.sisu.MainStage;
 import fi.tuni.prog3.sisu.NewStudentScene;
+import javafx.geometry.VerticalDirection;
 import javafx.scene.Node;
-import javafx.scene.control.Control;
-import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,13 +19,16 @@ import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 import org.testfx.assertions.api.WindowAssert;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.robot.Motion;
+import org.testfx.service.query.PointQuery;
+import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.api.FxAssert.verifyThatIter;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
 public class SisuTest extends ApplicationTest {
@@ -106,6 +109,64 @@ public class SisuTest extends ApplicationTest {
     @Test
     public void testCourseTabElements(){
 
+        FxRobot robot = new FxRobot();
+        goToMainMenu(robot);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        robot.clickOn("#designTab");
+        verifyThat("#designTab",Node::isVisible);
+
+        robot.clickOn("#courseTab");
+        verifyThat("#courseTab",Node::isVisible);
+
+        robot.clickOn("#personalTab");
+        verifyThat("#personalTab",Node::isVisible);
+
+    }
+
+    private void goToMainMenu(FxRobot robot){
+        robot.clickOn("#newStudentButton");
+
+        robot.clickOn("#nameField");
+        robot.write("Teemu Teekkari",1);
+
+        robot.clickOn("#studentNumberField");
+        robot.write("69696969",1);
+
+        robot.clickOn("#startingYearField");
+        robot.write("1999",1);
+
+        robot.clickOn("#degreeComboBox", Motion.DIRECT);
+        robot.moveBy(0,50);
+        robot.scroll(30,VerticalDirection.DOWN);
+        robot.clickOn(MouseButton.PRIMARY);
+
+        robot.clickOn("#nextButton");
+    }
+
+    @Test
+    public void testLogOut(){
+        FxRobot robot = new FxRobot();
+        goToMainMenu(robot);
+        robot.clickOn("#logOutLabel");
+        verifyThat("#logInLabel",Node::isVisible);
+    }
+
+    @ParameterizedTest
+    @MethodSource("goodInputArgumentProvider")
+    public void testPersonalTabElement(String s1,String s2,String s3){
+        FxRobot robot = new FxRobot();
+        createNewStudentWithUi(s1,s2,s3,robot);
+        clickOn("#personalTab");
+        WaitForAsyncUtils.waitForFxEvents();
+        verifyThat("#nameInfoLabel",hasText("Koko nimi"));
+        verifyThat("#nameLabel",hasText(s1));
+        verifyThat("#studentNumberInfoLabel",hasText("Opiskelijanumero"));
+        verifyThat("#studentNumberLabel",hasText(s2));
+        verifyThat("#emailInfoLabel",hasText("Sähköpostiosoite"));
+        // TODO:
+        //  verifyThat("#emailLabel",hasText(<email>));
+
     }
 
 
@@ -113,48 +174,67 @@ public class SisuTest extends ApplicationTest {
 
     //TODO: Robottitestit
     @ParameterizedTest
-    @MethodSource("argumentProvider")
+    @MethodSource("badInputArgumentProvider")
     public void testInputsNewStudentScene(String s1,String s2,String s3){
         FxRobot robot = new FxRobot();
+        createNewStudentWithUi(s1,s2,s3,robot);
+
+        if ("".equals(s1) || "".equals(s2)  || "".equals(s3)){
+            verifyThat("#notFilledAllInfoLabel",Node::isVisible);
+        }
+
+        else {
+            verifyThat("#menuBox",Node::isVisible);
+
+        }
+
+    }
+    private void createNewStudentWithUi(String name,String studentNumber,
+                                        String startingYear,FxRobot robot){
         robot.clickOn("#newStudentButton");
 
         robot.clickOn("#nameField");
-        robot.write(s1,20);
+        robot.write(name);
 
         robot.clickOn("#studentNumberField");
-        robot.write(s2,20);
+        robot.write(studentNumber);
 
         robot.clickOn("#startingYearField");
-        robot.write(s3,20);
+        robot.write(startingYear);
+
+        robot.clickOn("#degreeComboBox", Motion.DIRECT);
+        robot.moveBy(0,50);
+        robot.scroll(VerticalDirection.DOWN);
+        robot.clickOn(MouseButton.PRIMARY);
 
         robot.clickOn("#nextButton");
-        // Jotain tapahtuu mitä ei pitäisi tapahtua
-        var notNull1 = fromAll().lookup("#alreadyUsedInfoLabel");
-        var notNull2 = fromAll().lookup("#notFilledAllInfoLabel");
 
-
-        assertNotNull(notNull1);
-        assertNotNull(notNull2);
-
+        // Made the robot to wait for a bit because basic wait()
+        // method did not work as intended
+        WaitForAsyncUtils.waitForFxEvents();
     }
+
     @Test
     public void testAllElementsOneByOne(){
         FxRobot robot = new FxRobot();
 
     }
 
-    static Stream<Arguments> argumentProvider(){
+    static Stream<Arguments> badInputArgumentProvider(){
         return Stream.of(
-                arguments("Onni Merilä","H299725","2020")
+                arguments("Onni Merilä","H299725","2020"),
+                arguments("","",""),
+                arguments("Onni","","2020"),
+                arguments("Onni","H299725","")
         );
     }
 
 
-    static Stream<Arguments> argumentListProvider(){
+    static Stream<Arguments> goodInputArgumentProvider(){
         return Stream.of(
-                arguments(new String[]{}
-                )
-
+                arguments("Onni Merilä","H299725","2020"),
+                arguments("Pertti Perälä","12222222","1982"),
+                arguments("Jeesus Kristus","66642069","2001")
         );
     }
 
