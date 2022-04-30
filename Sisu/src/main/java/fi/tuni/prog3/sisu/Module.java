3bebb001095a97251cf061cde4b1ff716b91ed73
Onni Merilä <onni.merila@gmail.com>
2022-04-30 04:36:52 +0300

    Cleaned up Module -Onni

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Module.java
@@ -1,11 +1,19 @@
 package fi.tuni.prog3.sisu;
 
-import com.google.gson.JsonArray;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Module {
+
+    private final String moduleName;
+    private final String moduleCode;
+    private ArrayList<StudyModule> studyModules;
+
+    public Module(String moduleName, String moduleCode, ArrayList<StudyModule> studyModules) {
+        this.moduleName = moduleName;
+        this.moduleCode = moduleCode;
+        this.studyModules = studyModules;
+    }
+
     public String getModuleName() {
         return moduleName;
     }
@@ -18,24 +26,8 @@ public class Module {
         return studyModules;
     }
 
-    private final String moduleName;
-    private final String moduleCode;
-
     public void setStudyModules(StudyModule studyModule) {
         this.studyModules.add(studyModule);
     }
-
-    private ArrayList<StudyModule> studyModules;
-
-
-    public Module(String moduleName, String moduleCode, ArrayList<StudyModule> studyModules) {
-        this.moduleName = moduleName;
-        this.moduleCode = moduleCode;
-        this.studyModules = studyModules;
-    }
-
-
-
-
 }
 
